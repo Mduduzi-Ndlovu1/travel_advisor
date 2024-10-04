268bd1aa5f8979329e0ce18fbd72e60d81f161e1
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-10-04 16:25:00 +0200

    feat: integrate dynamic map with places markers
    - Implemented `GoogleMapReact` component to display an interactive map with user coordinates.
    - Added dynamic markers for `places` based on latitude and longitude using Google Maps.
    - Enabled user to view place details (name, image, rating) in a responsive `Paper` popup on larger screens, and marker icons for mobile.
    - Updated map to change the center and bounds dynamically based on user interaction.
    - Used Material UI `Typography`, `Rating`, and `Paper` components to display place information when clicking on map markers.
    - Introduced `useMediaQuery` to detect screen size and conditionally display place details on mobile and desktop views.

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -21,9 +21,9 @@ const PlaceDetails = ({place}) => {
           {place.name}
         </Typography>
         <Box display="flex" justifyContent="space-between">
-          <Typography variant="subtitle1">Price</Typography>
-          <Typography gutterBottom variant="subtitle1">
-            {place.price_level}
+        <Rating value={Number(place.rating)} readOnly />
+        <Typography gutterBottom variant="subtitle1">
+            Out of {place.num_reviews} reviews
           </Typography>
         </Box>
         <Box display="flex" justifyContent="space-between">
