08da8c396d878aae987dbbf8a8c8175b8c0465b0
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-10-08 14:56:56 +0200

    Added Google Maps integration with autocomplete and location search functionality
    - Integrated Google Maps API with `Autocomplete` for location search in the Header component
    - Added functionality to update map coordinates based on selected place from autocomplete
    - Implemented geolocation to set default coordinates on page load
    - Filtered places based on user rating and map bounds
    - Updated Map component to display places with location markers and details in map view
    - Fixed autocomplete initialization logic and ensured API loads correctly before usage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search'
 import useStyles from './styles';
-const Header = () => {
+
+
+const Header = ({ setCoordinates }) => {
   const classes = useStyles();
+  const [autocomplete, setAutocomplete] = useState(null);
+  
+
+   const onPlaceChanged = () => {
+    if (autocomplete) {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        const lat = place.geometry.location.lat();
+        const lng = place.geometry.location.lng();
+        console.log(lat, lng);
+        setCoordinates({ lat, lng });
+      } else {
+        console.log("No geometry found for the selected place.");
+      }
+    } else {
+      console.log("Autocomplete is not loaded yet.");
+    }
+  };
+  
+  const onLoad = (autoC) => {
+    setAutocomplete(autoC);
+    console.log("Autocomplete loaded");
+  };
+  
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -13,9 +39,9 @@ const Header = () => {
         </Typography>
         <Box display="flex">
           <Typography variant='h6' className={classes.title}>
-            Restaurants, Hotels & Attractions around you!!!
+            Restaurants, Hotels & Attractions Around you!!!
           </Typography>
-          {/* <Autocomplete> */}
+           <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged} className={classes.autocomplete}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
@@ -25,7 +51,7 @@ const Header = () => {
                   input: classes.inputInput
                 } } />
             </div>
-          {/* </Autocomplete> */}
+           </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
